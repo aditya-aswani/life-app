2f986e1bc65cd5154b1f2a0d6075072b8deaf3ed
aditya-aswani <adityaaswani1@gmail.com>
2024-03-24 00:40:28 -0400

    more text

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
-print("Welcome to Life!")
+print("\n*Welcome to Life!*\n\n*Here are your current stats:*")
+
+print("\nYour overall Level is 1!")
+
+print("\n*Here are your levels across specific skills:*")
+
+print("\nYour Meditation Level is 1!")
+
+print("\nYour Coding Level is 1!")
+
+print("\nYour Pull Up Level is 1!")
+
+
 
